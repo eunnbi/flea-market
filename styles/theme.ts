a9decce487c10ea8c089c40f83f416e808ecb2bd
Eunbi Kang <jenabill@naver.com>
2022-11-16 05:18:20 +0900

    💄 add theme for mui

---
+++ styles/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  ...createTheme(),
+  palette: {
+    primary: {
+      main: '#000000',
+      dark: '#000000',
+      light: '#000000',
+    },
+    secondary: {
+      main: '#1976d2',
+      dark: '#1565c0',
+      light: '#42a5f5',
+    },
+  },
+});
