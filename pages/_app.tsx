9e1e819e05cb41c5234990c35f78557f9e281edc
Eunbi Kang <jenabill@naver.com>
2022-11-16 19:53:11 +0900

    ✨ add redirect logic according to user's role

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,50 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Header from '@components/Header';
+import type { AppContext, AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material';
 import { theme } from 'styles/theme';
+import cookies from 'next-cookies';
+import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
+import { User } from '@prisma/client';
+import { getRedirectInfo } from '@lib/getRedirectInfo';
+import Header from '@components/common/Header';
+import LoginForm from '@components/LoginForm';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { id, query } = pageProps;
+  const { login } = query;
   return (
     <ThemeProvider theme={theme}>
-      <Header />
+      <Header isLogin={id} />
       <Component {...pageProps} />
+      {!id && login && <LoginForm />}
     </ThemeProvider>
   );
 }
+
+App.getInitialProps = async ({ ctx, Component }: AppContext) => {
+  const { id } = cookies(ctx);
+  console.log(id);
+  const baseUrl = ctx.req ? getAbsoluteUrl(ctx.req) : '';
+  if (id) {
+    const response = await fetch(`${baseUrl}/api/user?id=${id}`, {
+      method: 'GET',
+    });
+    const user: User = await response.json();
+    const info = getRedirectInfo(ctx.pathname, user.role);
+    console.log(ctx.pathname, info, user.role);
+    if (info && ctx.res) {
+      ctx.res.writeHead(302, { Location: info.destination });
+      ctx.res.end();
+    }
+  } else if ((ctx.pathname === '/sell' || ctx.pathname === '/admin') && ctx.res) {
+    ctx.res.writeHead(302, { Location: '/' });
+    ctx.res.end();
+  }
+  return {
+    pageProps: {
+      pathname: ctx.pathname,
+      query: ctx.query,
+      id,
+    },
+  };
+};
