91216fba14f0020c3e81eca5ba2f5d336985e0a1
Eunbi Kang <jenabill@naver.com>
2022-11-19 18:11:30 +0900

    ✨ add ImageUpload component

---
+++ components/ImageUpload.tsx
@@ -0,0 +1,68 @@
+import { MdOutlineAddPhotoAlternate } from 'react-icons/md';
+import { useRef, useState } from 'react';
+import styled from '@emotion/styled';
+import { noError } from '@lib/formValidation';
+
+interface Props {
+  imageFile: any;
+  changeImageFile: (e: any) => void;
+  errorInfo: typeof noError;
+}
+
+const ImageUpload = ({ imageFile, changeImageFile, errorInfo }: Props) => {
+  const ref = useRef<HTMLInputElement | null>(null);
+  return (
+    <Wrapper>
+      {imageFile && (
+        <div className="w-full">
+          <img src={URL.createObjectURL(imageFile)} />
+        </div>
+      )}
+      <div>
+        <button onClick={() => ref.current?.click()} type="button">
+          <MdOutlineAddPhotoAlternate />
+          사진 업로드
+        </button>
+        <input type="file" accept="image/*" ref={ref} onChange={changeImageFile} />
+        {errorInfo.isError && <p className="warning">{errorInfo.message}</p>}
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  position: relative;
+  input {
+    display: none;
+  }
+  svg {
+    font-size: 1.5rem;
+  }
+  button {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    border: 2px solid #000;
+    border-radius: 5px;
+    padding: 1rem;
+  }
+  p {
+    text-align: center;
+  }
+  img {
+    max-width: 100%;
+  }
+  p.warning {
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    font-size: 0.75rem;
+    line-height: 1.66;
+    letter-spacing: 0.03333em;
+  }
+`;
+
+export default ImageUpload;
