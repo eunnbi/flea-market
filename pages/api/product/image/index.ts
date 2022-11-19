3908e924a1b4563a19a810971a390c05ad4e9a90
Eunbi Kang <jenabill@naver.com>
2022-11-19 21:01:20 +0900

    🚩 feature: image create api

---
+++ pages/api/product/image/index.ts
@@ -0,0 +1,19 @@
+import { createImage } from '@db/image';
+import prisma from '@db/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+//@ts-ignore
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    switch (req.method) {
+      case 'POST': {
+        const image = await createImage(req.body);
+        return res.status(200).json({ success: true, image });
+      }
+      default:
+        break;
+    }
+  } catch (error: any) {
+    return res.status(500).json({ ...error, message: error.message });
+  }
+}
