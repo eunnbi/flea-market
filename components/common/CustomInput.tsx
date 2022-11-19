8753d7b0862804c10d2fd987f45ac9537672867d
Eunbi Kang <jenabill@naver.com>
2022-11-19 18:11:16 +0900

    ♻️ refactor: CustomInput component

--- components/common/CustomInput.tsx
+++ components/common/CustomInput.tsx
@@ -1,21 +1,36 @@
 import { FormControl, InputLabel, Input, IconButton, InputAdornment, FormHelperText } from '@mui/material';
 import { ChangeEventHandler, useState } from 'react';
 import { BsEyeSlashFill, BsEyeFill } from 'react-icons/bs';
+import { IconType } from 'react-icons/lib';
 
 interface Props {
+  type?: string;
   label: string;
   isPassword: boolean;
   htmlFor: string;
   onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
-  value: string;
+  value: string | number;
   errorInfo: {
     isError: boolean;
     message: string;
   };
   helperText?: string;
+  disabled?: boolean;
+  icon?: React.ReactNode;
 }
 
-const CustomInput = ({ label, isPassword, htmlFor, onChange, value, helperText, errorInfo }: Props) => {
+const CustomInput = ({
+  type,
+  label,
+  isPassword,
+  htmlFor,
+  onChange,
+  value,
+  helperText,
+  errorInfo,
+  disabled,
+  icon,
+}: Props) => {
   const { isError, message } = errorInfo;
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => {
@@ -25,7 +40,7 @@ const CustomInput = ({ label, isPassword, htmlFor, onChange, value, helperText, 
     event.preventDefault();
   };
   return (
-    <FormControl sx={{ m: 1, width: '100%', margin: 0 }} variant="standard">
+    <FormControl sx={{ m: 1, width: '100%', margin: 0 }} variant="standard" disabled={disabled}>
       <InputLabel htmlFor={htmlFor} error={isError}>
         {label}
       </InputLabel>
@@ -48,8 +63,17 @@ const CustomInput = ({ label, isPassword, htmlFor, onChange, value, helperText, 
             </InputAdornment>
           }
         />
+      ) : icon ? (
+        <Input
+          id={htmlFor}
+          type={type ? type : 'text'}
+          value={value}
+          onChange={onChange}
+          error={isError}
+          endAdornment={icon}
+        />
       ) : (
-        <Input id={htmlFor} type="text" value={value} onChange={onChange} error={isError} />
+        <Input id={htmlFor} type={type ? type : 'text'} value={value} onChange={onChange} error={isError} />
       )}
       {helperText ? <FormHelperText>{helperText}</FormHelperText> : null}
       {message ? <FormHelperText error>{message}</FormHelperText> : null}
