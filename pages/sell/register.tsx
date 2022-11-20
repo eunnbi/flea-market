a5dba9c412b1668f63a88f70be1280e56df6f610
Eunbi Kang <jenabill@naver.com>
2022-11-21 01:21:50 +0900

    👔  update register page logic

--- pages/sell/register.tsx
+++ pages/sell/register.tsx
@@ -1,21 +1,43 @@
 import CustomHead from '@components/common/CustomHead';
 import ProductRegisterForm from '@components/ProductRegisterForm';
 import styled from '@emotion/styled';
+import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 
-const ProductRegister = () => {
+const ProductRegister = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <CustomHead title="Register Product" />
       <Main>
         <h2>Product Registration</h2>
-        <ProductRegisterForm />
+        <ProductRegisterForm initialProduct={data} />
       </Main>
     </>
   );
 };
 
 const Main = styled.main`
-  padding: 2rem 4rem;
+  padding: 2rem;
+  h2 {
+    margin-bottom: 1.5rem;
+  }
 `;
+
+export const getServerSideProps = async ({ query, req }: GetServerSidePropsContext) => {
+  if (query.id) {
+    const baseUrl = getAbsoluteUrl(req);
+    const res = await fetch(`${baseUrl}/api/product?id=${query.id}`);
+    const data = await res.json();
+    return {
+      props: {
+        data,
+      },
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
+};
 
 export default ProductRegister;
