33949f85722f039fd973b9174a8996730d9f5b21
Eunbi Kang <jenabill@naver.com>
2022-11-22 01:32:08 +0900

    🐛 fix: Map component with kakao map api

--- components/common/Map.tsx
+++ components/common/Map.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import Script from 'next/script';
 
 interface Props {
   location: string;
@@ -6,29 +7,11 @@ interface Props {
 }
 
 function Map({ location, setLocationErrorInfo }: Props) {
-  const [position, setPosition] = useState({
-    latitude: 0,
-    longitude: 0,
-  });
-  const { latitude, longitude } = position;
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords }) =>
-      setPosition({
-        latitude: coords.latitude,
-        longitude: coords.longitude,
-      }),
-    );
-  }, []);
-
-  useEffect(() => {
-    const mapScript = document.createElement('script');
-    mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`;
-    document.head.appendChild(mapScript);
-
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        if (latitude === 0 || longitude === 0) return;
+  const onLoadKakaoMap = () => {
+    if (!window.kakao || !window.kakao.maps || !window.kakao.maps.services) return;
+    if (location === '') {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        const { latitude, longitude } = coords;
         const container = document.getElementById('map');
         const options = {
           center: new window.kakao.maps.LatLng(latitude, longitude),
@@ -41,31 +24,44 @@ function Map({ location, setLocationErrorInfo }: Props) {
         });
         marker.setMap(map);
       });
-    };
-
-    mapScript.addEventListener('load', onLoadKakaoMap);
-    return () => mapScript.removeEventListener('load', onLoadKakaoMap);
-  }, [latitude, longitude]);
+    } else {
+      const geocoder = new window.kakao.maps.services.Geocoder();
+      geocoder.addressSearch(location, function (result: any, status: any) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === window.kakao.maps.services.Status.OK) {
+          const container = document.getElementById('map');
+          const options = {
+            center: new window.kakao.maps.LatLng(result[0].y, result[0].x),
+            level: 3,
+          };
+          const map = new window.kakao.maps.Map(container, options);
+          const markerPosition = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+          const marker = new window.kakao.maps.Marker({
+            position: markerPosition,
+          });
+          marker.setMap(map);
+          setLocationErrorInfo(false);
+        } else {
+          setLocationErrorInfo(true);
+        }
+      });
+    }
+  };
 
   useEffect(() => {
-    if (location === '') return;
-    console.log(location);
-    const geocoder = new window.kakao.maps.services.Geocoder();
-    geocoder.addressSearch(location, function (result: any, status: any) {
-      // 정상적으로 검색이 완료됐으면
-      if (status === window.kakao.maps.services.Status.OK) {
-        setPosition({
-          latitude: result[0].y,
-          longitude: result[0].x,
-        });
-        setLocationErrorInfo(false);
-      } else {
-        setLocationErrorInfo(true);
-      }
-    });
+    onLoadKakaoMap();
   }, [location]);
 
-  return <div id="map" />;
+  return (
+    <>
+      <Script
+        src={`http://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`}
+        type="text/javascript"
+        onLoad={() => window.kakao.maps.load(onLoadKakaoMap)}
+      />
+      <div id="map" />
+    </>
+  );
 }
 
 export default React.memo(Map);
