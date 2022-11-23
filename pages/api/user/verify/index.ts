4ba638bdabf7818adb3d06848da7f7d131a492a9
Eunbi Kang <jenabill@naver.com>
2022-11-24 03:11:26 +0900

    🚩 add user verify api

---
+++ pages/api/user/verify/index.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+import { User } from '@prisma/client';
+
+const KEY = String(process.env.JSON_KEY);
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    switch (req.method) {
+      case 'GET': {
+        if (!req.headers.authorization) {
+          return res.json({ verify: false, user: undefined });
+        } else {
+          const token = req.headers.authorization.split(' ')[1];
+          const decoded = jwt.verify(token, KEY);
+          const user: User = decoded as User;
+          return res.status(200).json({ verify: true, user });
+        }
+      }
+      default:
+        break;
+    }
+  } catch (error: any) {
+    return res.status(500).json({ ...error, message: error.message });
+  }
+}
