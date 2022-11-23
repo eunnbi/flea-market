144de8517d6fbda4dabcf989e0f8ab9d27c9d40e
Eunbi Kang <jenabill@naver.com>
2022-11-24 03:23:28 +0900

    🚩 update get user api

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getAllUsers, getUserById, updateUser } from '@db/user';
+import { getAllUsers, getUserById, getUsersByRole, updateUser } from '@db/user';
+import { Role } from '@prisma/client';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -10,6 +11,9 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
           // api/users?id=1
           const user = await getUserById(req.query.id as string);
           return res.status(200).json(user);
+        } else if (req.query.role) {
+          const users = await getUsersByRole(req.query.role as Role);
+          return res.status(200).json(users);
         } else {
           // Otherwise, fetch all users
           const users = await getAllUsers();
