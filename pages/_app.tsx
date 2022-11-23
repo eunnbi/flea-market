d2489f23eac70a3760caf49cbd46761465d089a5
Eunbi Kang <jenabill@naver.com>
2022-11-24 03:22:57 +0900

    ♻️ refactor: App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,15 @@ import { ThemeProvider } from '@mui/material';
 import { theme } from 'styles/theme';
 import cookies from 'next-cookies';
 import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
-import { User } from '@prisma/client';
 import { getRedirectInfo } from '@lib/getRedirectInfo';
 import Header from '@components/common/Header';
 import LoginForm from '@components/LoginForm';
 import { useEffect } from 'react';
 import Router from 'next/router';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { id, query, pathname } = pageProps;
+  const { verify, query, pathname } = pageProps;
   useEffect(() => {
     let timerId: NodeJS.Timeout;
     if (query && query.alert) {
@@ -28,24 +28,28 @@ export default function App({ Component, pageProps }: AppProps) {
     return () => clearTimeout(timerId);
   }, [query, pathname]);
   return (
-    <ThemeProvider theme={theme}>
-      <Header isLogin={id} />
-      <Component {...pageProps} />
-      {!id && query && query.login && <LoginForm />}
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <Header isLogin={verify} />
+        <Component {...pageProps} />
+        {!verify && query && query.login && <LoginForm />}
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
 App.getInitialProps = async ({ ctx }: AppContext) => {
-  const { id } = cookies(ctx);
+  const { access_token } = cookies(ctx);
   const baseUrl = ctx.req ? getAbsoluteUrl(ctx.req) : '';
-  if (id) {
-    const response = await fetch(`${baseUrl}/api/user?id=${id}`, {
-      method: 'GET',
-    });
-    const user: User = await response.json();
+  const response = await fetch(`${baseUrl}/api/user/verify`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+  const { verify, user } = await response.json();
+  if (verify) {
     const info = getRedirectInfo(ctx.pathname, user.role);
-    console.log(ctx.pathname, info, user.role);
     if (info && ctx.res) {
       ctx.res.writeHead(302, { Location: info.destination });
       ctx.res.end();
@@ -58,7 +62,7 @@ App.getInitialProps = async ({ ctx }: AppContext) => {
     pageProps: {
       pathname: ctx.pathname,
       query: ctx.query,
-      id,
+      verify,
     },
   };
 };
