42ce9cef09312f77a34a14d850354be8e62d3f22
Eunbi Kang <jenabill@naver.com>
2022-11-24 03:14:30 +0900

    👔 set authorization header

--- pages/sell/register.tsx
+++ pages/sell/register.tsx
@@ -2,9 +2,14 @@ import CustomHead from '@components/common/CustomHead';
 import ProductRegisterForm from '@components/ProductRegisterForm';
 import styled from '@emotion/styled';
 import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
+import axios from 'axios';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import cookies from 'next-cookies';
 
-const ProductRegister = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const ProductRegister = ({ data, access_token }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (access_token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+  }
   return (
     <>
       <CustomHead title="Register Product" />
@@ -23,7 +28,9 @@ const Main = styled.main`
   }
 `;
 
-export const getServerSideProps = async ({ query, req }: GetServerSidePropsContext) => {
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const { access_token } = cookies(ctx);
+  const { query, req } = ctx;
   if (query.id) {
     const baseUrl = getAbsoluteUrl(req);
     const res = await fetch(`${baseUrl}/api/product?id=${query.id}`);
@@ -36,7 +43,8 @@ export const getServerSideProps = async ({ query, req }: GetServerSidePropsConte
   } else {
     return {
       props: {
-        data: undefined,
+        data: null,
+        access_token,
       },
     };
   }
