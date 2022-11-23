b776b3833c0816e761974b90a26b8fa498a18607
Eunbi Kang <jenabill@naver.com>
2022-11-24 03:11:59 +0900

    ✨ add StatusFilter component

---
+++ components/StatusFilter.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import { Chip } from '@mui/material';
+import React, { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+  filter: {
+    AUCTION: boolean;
+    PURCHASED: boolean;
+    PROGRESS: boolean;
+  };
+  setFilter: Dispatch<
+    SetStateAction<{
+      AUCTION: boolean;
+      PURCHASED: boolean;
+      PROGRESS: boolean;
+    }>
+  >;
+}
+
+const StatusFilter = ({ filter, setFilter }: Props) => {
+  return (
+    <Div>
+      <Chip
+        label="경매"
+        onClick={() => setFilter(filter => ({ ...filter, AUCTION: !filter.AUCTION }))}
+        variant={filter.AUCTION ? 'filled' : 'outlined'}
+      />
+      <Chip
+        label="판매 진행 중"
+        onClick={() => setFilter(filter => ({ ...filter, PROGRESS: !filter.PROGRESS }))}
+        variant={filter.PROGRESS ? 'filled' : 'outlined'}
+      />
+      <Chip
+        label="판매 완료"
+        onClick={() => setFilter(filter => ({ ...filter, PURCHASED: !filter.PURCHASED }))}
+        variant={filter.PURCHASED ? 'filled' : 'outlined'}
+      />
+    </Div>
+  );
+};
+
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export default React.memo(StatusFilter);
