00813c7ca8cd71d38150c304ffcafb49fa0e8192
Eunbi Kang <jenabill@naver.com>
2022-11-25 00:31:50 +0900

    🚚 rename: SimpleDialog component

--- components/common/SimpleDialog.tsx
+++ components/common/SimpleDialog.tsx
@@ -4,14 +4,17 @@ import { useState } from 'react';
 interface Props {
   open: boolean;
   handleClose: () => void;
-  onDelete: () => Promise<void>;
+  onConfirm: () => Promise<void>;
+  basicTitle: string;
+  loadingTitle: string;
+  content: string;
 }
 
-export const DeleteDialog = ({ open, handleClose, onDelete }: Props) => {
+const SimpleDialog = ({ open, handleClose, onConfirm, basicTitle, loadingTitle, content }: Props) => {
   const [loading, setLoading] = useState(false);
-  const onConfirm = () => {
+  const onClickOkButton = () => {
     setLoading(true);
-    onDelete().then(() => {
+    onConfirm().then(() => {
       setLoading(false);
       handleClose();
     });
@@ -22,18 +25,20 @@ export const DeleteDialog = ({ open, handleClose, onDelete }: Props) => {
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description">
-      <DialogTitle id="alert-dialog-title">{loading ? '삭제 중...' : '정말 삭제하시겠습니까?'}</DialogTitle>
+      <DialogTitle id="alert-dialog-title">{loading ? loadingTitle : basicTitle}</DialogTitle>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description"></DialogContentText>
+        <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="error" disabled={loading}>
           취소
         </Button>
-        <Button onClick={onConfirm} autoFocus color="secondary" disabled={loading}>
+        <Button onClick={onClickOkButton} autoFocus color="secondary" disabled={loading}>
           확인
         </Button>
       </DialogActions>
     </Dialog>
   );
 };
+
+export default SimpleDialog;
