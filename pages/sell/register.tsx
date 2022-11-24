76d45efde2a7fdb50553f70ac247c172e531258a
Eunbi Kang <jenabill@naver.com>
2022-11-25 00:50:00 +0900

    ♻️ refactor: sell register page

--- pages/sell/register.tsx
+++ pages/sell/register.tsx
@@ -1,18 +1,19 @@
 import CustomHead from '@components/common/CustomHead';
+import Header from '@components/common/Header';
 import ProductRegisterForm from '@components/ProductRegisterForm';
 import styled from '@emotion/styled';
 import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
 import axios from 'axios';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
-import cookies from 'next-cookies';
 
-const ProductRegister = ({ data, access_token }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  if (access_token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+const ProductRegister = ({ data, token }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   }
   return (
     <>
       <CustomHead title="Register Product" />
+      <Header isLogin={true} />
       <Main>
         <h2>Product Registration</h2>
         <ProductRegisterForm initialProduct={data} />
@@ -28,26 +29,49 @@ const Main = styled.main`
   }
 `;
 
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const { access_token } = cookies(ctx);
-  const { query, req } = ctx;
-  if (query.id) {
-    const baseUrl = getAbsoluteUrl(req);
-    const res = await fetch(`${baseUrl}/api/product?id=${query.id}`);
-    const data = await res.json();
-    return {
-      props: {
-        data,
-      },
-    };
-  } else {
-    return {
-      props: {
-        data: null,
-        access_token,
-      },
-    };
+export const getServerSideProps = async ({ req, query }: GetServerSidePropsContext) => {
+  const { cookies } = req;
+  const baseUrl = getAbsoluteUrl(req);
+  const res = await fetch(`${baseUrl}/api/user/verify`, {
+    headers: {
+      Authorization: cookies.access_token ? `Bearer ${cookies.access_token}` : 'Bearer',
+    },
+  });
+  const { verify, user } = await res.json();
+  if (verify) {
+    if (user.role === 'ADMIN') {
+      return {
+        redirect: {
+          destination: '/admin',
+          permanent: false,
+        },
+      };
+    }
+    if (user.role === 'SELLER') {
+      if (query.id) {
+        const res = await fetch(`${baseUrl}/api/product?id=${query.id}`);
+        const data = await res.json();
+        return {
+          props: {
+            data,
+          },
+        };
+      } else {
+        return {
+          props: {
+            data: null,
+            token: cookies.access_token,
+          },
+        };
+      }
+    }
   }
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false,
+    },
+  };
 };
 
 export default ProductRegister;
