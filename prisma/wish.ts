d30230ec53c1c7204e6d12ac1f7272e6c9794c85
Eunbi Kang <jenabill@naver.com>
2022-11-25 00:50:57 +0900

    🚩 add prisma wish query

---
+++ prisma/wish.ts
@@ -0,0 +1,52 @@
+import { Wish } from '@prisma/client';
+import prisma from './prisma';
+import { getProductById } from './product';
+
+export const createWish = async ({ buyerId, productId }: Pick<Wish, 'buyerId' | 'productId'>) => {
+  const res = await prisma.wish.create({
+    data: {
+      buyerId,
+      productId,
+    },
+  });
+  return res;
+};
+
+export const deleteWish = async ({ buyerId, productId }: Pick<Wish, 'buyerId' | 'productId'>) => {
+  const wish = await prisma.wish.findFirst({
+    where: { buyerId, productId },
+  });
+  if (wish === null) return wish;
+  else {
+    const res = await prisma.wish.delete({
+      where: { id: wish.id },
+    });
+    return res;
+  }
+};
+
+export const getWishList = async (buyerId: Wish['buyerId']) => {
+  const wish = await prisma.wish.findMany({
+    where: { buyerId },
+  });
+  const res = await Promise.all(
+    wish.map(item => {
+      return getProductById(item.productId, undefined, false);
+    }),
+  );
+  return res;
+};
+
+export const deleteWishByUser = async (buyerId: Wish['buyerId']) => {
+  const res = await prisma.wish.deleteMany({
+    where: { buyerId },
+  });
+  return res;
+};
+
+export const getWish = async ({ buyerId, productId }: Pick<Wish, 'buyerId' | 'productId'>) => {
+  const res = await prisma.wish.findFirst({
+    where: { buyerId, productId },
+  });
+  return res;
+};
