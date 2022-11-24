831e04712cc79e6332a2ad916e7b0a02d42ce39b
Eunbi Kang <jenabill@naver.com>
2022-11-25 00:39:16 +0900

    ♻️ refactor: admin page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,14 +1,16 @@
 import MemberTable from '@components/admin/MemberTable';
 import CustomHead from '@components/common/CustomHead';
+import Header from '@components/common/Header';
 import styled from '@emotion/styled';
 import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
 import { User } from '@prisma/client';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 
-const Admin = ({ members }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Admin = ({ members, isLogin }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <CustomHead title="Dashboard" />
+      <Header isLogin={isLogin} />
       <Main>
         <h1>Member Management</h1>
         <MemberTable initialMembers={members} />
@@ -29,15 +31,35 @@ const Main = styled.main`
 export default Admin;
 
 export const getServerSideProps = async ({ req }: GetServerSidePropsContext) => {
+  const { cookies } = req;
   const baseUrl = getAbsoluteUrl(req);
-  const response = await fetch(`${baseUrl}/api/user`, {
-    method: 'GET',
+  const res = await fetch(`${baseUrl}/api/user/verify`, {
+    headers: {
+      Authorization: cookies.access_token ? `Bearer ${cookies.access_token}` : 'Bearer ',
+    },
   });
-  const members: User[] = await response.json();
-
-  // Convert the updatedAt and createdAt in each user to string
-  // Otherwise, Next.js will throw an error
-  // Not required if you are not using the date fields
-
-  return { props: { members } };
+  const { verify, user } = await res.json();
+  if (verify) {
+    if (user.role === 'ADMIN') {
+      const response = await fetch(`${baseUrl}/api/user`, {
+        method: 'GET',
+      });
+      const members: User[] = await response.json();
+      return { props: { members, isLogin: verify } };
+    }
+    if (user.role === 'SELLER') {
+      return {
+        redirect: {
+          destination: '/sell',
+          permanent: false,
+        },
+      };
+    }
+  }
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false,
+    },
+  };
 };
