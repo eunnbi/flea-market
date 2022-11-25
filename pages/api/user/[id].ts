96c8f73f5272c57f244c74d4c91e8ff17c8a2cd5
Eunbi Kang <jenabill@naver.com>
2022-11-26 00:37:11 +0900

    🚩 update user delete api

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,5 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '@db/user';
+import { deleteProductsBySeller, getProductBySeller } from '@db/product';
+import { deleteImage } from '@db/image';
+import { deleteShoppingByUser } from '@db/shopping';
+import { deleteWishByUser } from '@db/wish';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -8,6 +12,19 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
         // Delete an existing user
         const { id } = req.query;
         const user = await deleteUser(id as string);
+
+        if (user.role === 'SELLER') {
+          // delete products of user
+          const products = await getProductBySeller(id as string);
+          await deleteProductsBySeller(id as string);
+          products.forEach(async ({ imageId }) => {
+            await deleteImage(imageId);
+          });
+        } else if (user.role === 'BUYER') {
+          // delete data at bidding, shopping, and wishlist table
+          await deleteShoppingByUser(id as string);
+          await deleteWishByUser(id as string);
+        }
         return res.json(user);
       }
       case 'PATCH': {
