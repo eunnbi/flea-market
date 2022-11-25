fb9a029ac02d98eab5929faaf32162aa83fe8ffc
Eunbi Kang <jenabill@naver.com>
2022-11-26 00:36:49 +0900

    🚩 update wishlist get api

--- pages/api/product/wish/index.ts
+++ pages/api/product/wish/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
 import { User } from '@prisma/client';
 import { getWishList } from '@db/wish';
+import { updateAuctionProduct } from '@db/product';
 
 const KEY = String(process.env.JSON_KEY);
 
@@ -12,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         if (!req.headers.authorization) {
           return res.status(200).json({ success: false, message: 'No Access Token' });
         } else {
+          await updateAuctionProduct();
           const token = req.headers.authorization.split(' ')[1];
           const decoded = jwt.verify(token, KEY);
           const { userId }: User = decoded as User;
