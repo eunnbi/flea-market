ae20e1f43fc898f847d3189175e08d4e0a3d037c
Eunbi Kang <jenabill@naver.com>
2022-11-28 00:45:30 +0900

    ♻️ refactor: CustomHead component

--- components/common/CustomHead.tsx
+++ components/common/CustomHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function CustomHead({ title }: { title: string }) {
   return (
     <Head>
-      <title>{`${title} | Flea Market`}</title>
+      <title>{`Flea Market`}</title>
     </Head>
   );
 }
