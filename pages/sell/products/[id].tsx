3d6be2742047c97932996ba00e472f1b0d50726a
Eunbi Kang <jenabill@naver.com>
2022-11-27 18:18:53 +0900

    ✨ update seller's home page

--- pages/sell/products/[id].tsx
+++ pages/sell/products/[id].tsx
@@ -8,14 +8,18 @@ import { FaRegComment } from 'react-icons/fa';
 import { IoMdHeartEmpty } from 'react-icons/io';
 import { BsCalendarDate } from 'react-icons/bs';
 import { Button, Chip } from '@mui/material';
-import { DeleteDialog } from '@components/common/DeleteDialog';
+import SimpleDialog from '@components/common/SimpleDialog';
 import axios from 'axios';
 import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import Header from '@components/common/Header';
+import { getDiffDay } from '@lib/getDiffDay';
+import AuctionHistory from '@components/AuctionHistory';
+import { RiHistoryLine } from 'react-icons/ri';
 
 const ProductDetail = ({ product }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const [open, setOpen] = useState(false);
-  const { id, name, price, tradingPlace, endingAt, status, image, content, likeCnt, phoneNumber } = product;
+  const { id, name, price, tradingPlace, endingAt, status, image, content, likeCnt, phoneNumber, bid } = product;
   const onDelete = async () => {
     try {
       await axios.delete(`/api/product/${id}`);
@@ -27,37 +31,48 @@ const ProductDetail = ({ product }: InferGetServerSidePropsType<typeof getServer
   return (
     <>
       <CustomHead title="Product Name" />
+      <Header isLogin={true} />
       <main className={styles.main}>
         <section>
           <img src={getImageUrl(image)} alt="product" />
           <h2>{name}</h2>
-          {status != 'AUCTION' && <p>{price.toLocaleString()}원</p>}
+          {status != 'AUCTION' && <p className={styles.price}>{price.toLocaleString()}원</p>}
         </section>
-        <section>
-          <Chip label={status === 'AUCTION' ? '경매' : status === 'PROGRESS' ? '판매 진행중' : '판매 완료'} />
+        <section className={styles.contentBox}>
+          <div className={styles.row}>
+            <Chip label={status === 'AUCTION' ? '경매' : status === 'PROGRESS' ? '판매 진행중' : '판매 완료'} />
+            {endingAt && <Chip label={`D-${getDiffDay(String(endingAt))}`} />}
+          </div>
           {endingAt && (
-            <p>
+            <p className={styles.content}>
               <BsCalendarDate />
               <span>{String(endingAt).split('T')[0]}</span>
             </p>
           )}
-          <p>
+          <p className={styles.content}>
             <IoLocationOutline />
             {tradingPlace}
           </p>
-          <p>
+          <p className={styles.content}>
             <IoCallOutline />
             {phoneNumber}
           </p>
-          <p>
+          <p className={styles.content}>
             <FaRegComment />
             {content}
           </p>
+          {status === 'AUCTION' && (
+            <div className={styles.content}>
+              <RiHistoryLine />
+              <div className={styles.bidTable}>
+                <span>입찰목록 ({bid.length})</span>
+                <AuctionHistory history={bid} />
+              </div>
+            </div>
+          )}
         </section>
-        <p className="likeCnt">
-          <button>
-            <IoMdHeartEmpty className="heart_icon" />
-          </button>
+        <p className={styles.likeCnt}>
+          <IoMdHeartEmpty className="heart_icon" />
           {likeCnt}
         </p>
         <div>
@@ -81,18 +96,50 @@ const ProductDetail = ({ product }: InferGetServerSidePropsType<typeof getServer
           </Button>
         </div>
       </main>
-      <DeleteDialog open={open} handleClose={() => setOpen(false)} onDelete={onDelete} />
+      <SimpleDialog
+        open={open}
+        handleClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        basicTitle="정말 삭제하시겠습니까?"
+        loadingTitle="삭제 중..."
+        content=""
+      />
     </>
   );
 };
 
 export const getServerSideProps = async ({ req, query }: GetServerSidePropsContext) => {
+  const { cookies } = req;
   const baseUrl = getAbsoluteUrl(req);
-  const response = await fetch(`${baseUrl}/api/product?id=${query.id as string}`);
-  const product = await response.json();
+  const res = await fetch(`${baseUrl}/api/user/verify`, {
+    headers: {
+      Authorization: cookies.access_token ? `Bearer ${cookies.access_token}` : 'Bearer',
+    },
+  });
+  const { verify, user } = await res.json();
+  if (verify) {
+    if (user.role === 'ADMIN') {
+      return {
+        redirect: {
+          destination: '/admin',
+          permanent: false,
+        },
+      };
+    }
+    if (user.role === 'SELLER') {
+      const response = await fetch(`${baseUrl}/api/product?id=${query.id as string}`);
+      const product = await response.json();
+      return {
+        props: {
+          product,
+        },
+      };
+    }
+  }
   return {
-    props: {
-      product,
+    redirect: {
+      destination: '/',
+      permanent: false,
     },
   };
 };
