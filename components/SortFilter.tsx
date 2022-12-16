ba6a38d988624b4254529e611ef883e854a88d52
Eunbi <jenabill@naver.com>
2022-12-16 22:41:03 +0900

    ♻️ refactor: SortFilter component

--- components/SortFilter.tsx
+++ components/SortFilter.tsx
@@ -1,19 +1,16 @@
-import { IconButton, Menu, MenuItem } from '@mui/material';
-import { BiSortDown } from 'react-icons/bi';
-import React, { useEffect, useState } from 'react';
-import styled from '@emotion/styled';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { sortFilterState } from '@store/sortFilterState';
-import { productsState } from '@store/productsState';
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import { BiSortDown } from "react-icons/bi";
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { useRecoilState } from "recoil";
+import { sortFilterState } from "@store/sortFilterState";
 
 const SortFilter = () => {
-  const setProducts = useSetRecoilState(productsState);
   const [sort, setSort] = useRecoilState(sortFilterState);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
-    
   };
   const handleClose = (e: React.MouseEvent<HTMLLIElement>) => {
     if (e.currentTarget.textContent) {
@@ -21,34 +18,16 @@ const SortFilter = () => {
     }
     setAnchorEl(null);
   };
-  useEffect(() => {
-    setProducts(({ initialProducts }) => sort === "최신순" ? ({ initialProducts, products: [...initialProducts].sort(({ createdAt: a }, { createdAt: b }) => {
-      if (a < b) {
-        return 1;
-      } else if (a > b) {
-        return -1;
-      } else {
-        return 0;
-      }
-    })}) : ({ initialProducts, products: [...initialProducts].sort(({ likeCnt: a }, { likeCnt: b }) => {
-      if (a < b) {
-        return 1;
-      } else if (a > b) {
-        return -1;
-      } else {
-        return 0;
-      }
-    })}))
-  }, [sort]);
   return (
     <Div>
       <span>{sort}</span>
       <IconButton
         id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
+        aria-controls={open ? "basic-menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}>
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
         <BiSortDown />
       </IconButton>
       <Menu
@@ -57,8 +36,9 @@ const SortFilter = () => {
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}>
+          "aria-labelledby": "basic-button",
+        }}
+      >
         <MenuItem onClick={handleClose}>최신순</MenuItem>
         <MenuItem onClick={handleClose}>좋아요순</MenuItem>
       </Menu>
