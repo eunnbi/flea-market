cf6b53fc1d993b8c22180c40ec3b3498ee5fe704
Eunbi <jenabill@naver.com>
2022-12-16 21:21:19 +0900

    ♻️ refactor: MemberTable component

--- components/admin/MemberTable.tsx
+++ components/admin/MemberTable.tsx
@@ -16,57 +16,38 @@ import { IoMdTrash } from 'react-icons/io';
 import { FaEdit } from 'react-icons/fa';
 import { tableCellClasses } from '@mui/material/TableCell';
 import { useState, useEffect } from 'react';
-import SimpleDialog from '../common/SimpleDialog';
-import EditDialog from './EditDialog';
-import axios from 'axios';
 import { BsArrowUpShort, BsArrowDownShort } from 'react-icons/bs';
-import Filter from './Filter';
 import { changeDateFormat } from '@lib/changeDateFormat';
-
-type State = Pick<User, 'userId' | 'firstName' | 'lastName' | 'role'>;
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { memberEditState } from '@store/admin/memberEditState';
+import { memberDeleteState } from '@store/admin/memberDeleteState';
+import { membersState } from '@store/admin/membersState';
+import { roleFilterState } from '@store/admin/roleFilterState';
 
 const MemberTable = ({ initialMembers }: { initialMembers: User[] }) => {
-  const [roleFilter, setRoleFilter] = useState({
-    admin: true,
-    seller: true,
-    buyer: true,
-  });
-  const { admin, seller, buyer } = roleFilter;
   const [isAscending, setIsAscending] = useState(true);
-  const [message, setMessage] = useState('');
-  const [members, setMembers] = useState(initialMembers);
-  const [deleteState, setDeleteState] = useState({
-    open: false,
-    id: '',
-  });
-
-  const [editState, setEditState] = useState<{ open: boolean; id: string; state: State }>({
-    open: false,
-    id: '',
-    state: {
-      userId: '',
-      firstName: '',
-      lastName: '',
-      role: 'ADMIN',
-    },
-  });
+  const { admin, seller, buyer } = useRecoilValue(roleFilterState);
+  const [members, setMembers] = useRecoilState(membersState);
+  const setMemberDeleteState = useSetRecoilState(memberDeleteState);
+  const setMemberEditState = useSetRecoilState(memberEditState);
 
   const onClickDeleteButton = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setDeleteState({
+    setMemberDeleteState({
       open: true,
-      id: String(e.currentTarget.ariaLabel),
+      id: String(e.currentTarget.dataset.memberid),
     });
   };
 
   const onClickEditButton = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const id = String(e.currentTarget.ariaLabel);
+    const id = String(e.currentTarget.dataset.memberid);
+    console.log(id);
     const user = members.find(member => member.id === id);
     if (user === undefined) return;
     const { userId, firstName, lastName, role } = user;
-    setEditState({
+    setMemberEditState({
       open: true,
       id,
-      state: {
+      initialState: {
         userId,
         firstName,
         lastName,
@@ -75,127 +56,71 @@ const MemberTable = ({ initialMembers }: { initialMembers: User[] }) => {
     });
   };
 
-  const handleClose = (type: 'delete' | 'edit') => {
-    if (type === 'delete') {
-      return () =>
-        setDeleteState(state => ({
-          ...state,
-          open: false,
-        }));
-    } else {
-      return () =>
-        setEditState(state => ({
-          ...state,
-          open: false,
-        }));
-    }
-  };
-
-  const deleteMember = (id: string) => async () => {
-    await axios.delete(`/api/user/${id}`);
-    const { data } = await axios.get('/api/user');
-    setMembers(data);
-    setMessage('삭제가 완료되었습니다.');
-  };
-
-  const editMember = (id: string) => async (state: State) => {
-    await axios.patch(`/api/user/${id}`, state);
-    const { data } = await axios.get(`/api/user`);
-    setMembers(data);
-    setMessage('수정이 완료되었습니다.');
-  };
-
-  const onChangeFilter = (e: any) => {
-    const textContent: 'admin' | 'buyer' | 'seller' = e.target.textContent;
-    setRoleFilter(state => ({ ...state, [textContent]: !state[textContent] }));
-  };
-
   useEffect(() => {
-    if (message === '') return;
-    setTimeout(() => {
-      setMessage('');
-    }, 2000);
-  }, [message]);
+    setMembers(initialMembers);
+  }, [initialMembers]);
 
   return (
-    <>
-      {message && <Alert severity="success">{message}</Alert>}
-      <Filter roleFilter={roleFilter} onChangeFilter={onChangeFilter} />
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <StyledTableRow>
-              <StyledTableCell>ID</StyledTableCell>
-              <StyledTableCell>Name</StyledTableCell>
-              <StyledTableCell>Role</StyledTableCell>
-              <StyledTableCell onClick={() => setIsAscending(state => !state)}>
-                Create At<StyledButton>{isAscending ? <BsArrowDownShort /> : <BsArrowUpShort />}</StyledButton>
-              </StyledTableCell>
-              <StyledTableCell align="center">Actions</StyledTableCell>
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>
-            {members
-              .sort(({ createdAt: a }, { createdAt: b }) => {
-                if (a < b) {
-                  return isAscending ? 1 : -1;
-                } else if (a > b) {
-                  return isAscending ? -1 : 1;
-                } else {
-                  return 0;
-                }
-              })
-              .filter(member => {
-                if (admin && member.role === 'ADMIN') return true;
-                if (seller && member.role === 'SELLER') return true;
-                if (buyer && member.role === 'BUYER') return true;
-                return false;
-              })
-              .map(member => (
-                <StyledTableRow key={member.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <StyledTableCell component="th" scope="row">
-                    {member.userId}
-                  </StyledTableCell>
-                  <StyledTableCell sx={{ textTransform: 'capitalize' }}>
-                    {member.firstName} {member.lastName}
-                  </StyledTableCell>
-                  <StyledTableCell
-                    color={member.role === 'ADMIN' ? 'warning' : member.role === 'SELLER' ? 'info' : 'success'}>
-                    {member.role}
-                  </StyledTableCell>
-                  <StyledTableCell>{changeDateFormat(new Date(String(member.createdAt)))}</StyledTableCell>
-                  <StyledTableCell align="center">
-                    <Tooltip title="edit" arrow>
-                      <IconButton size="small" aria-label={member.id} onClick={onClickEditButton} className="margin">
-                        <FaEdit />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title="delete" arrow>
-                      <IconButton size="small" aria-label={member.id} onClick={onClickDeleteButton} className="margin">
-                        <IoMdTrash />
-                      </IconButton>
-                    </Tooltip>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <SimpleDialog
-        open={deleteState.open}
-        handleClose={handleClose('delete')}
-        onConfirm={deleteMember(deleteState.id)}
-        basicTitle="정말 삭제하시겠습니까?"
-        loadingTitle="삭제 중..."
-        content="유저 관련 데이터도 전부 삭제됩니다."
-      />
-      <EditDialog
-        open={editState.open}
-        handleClose={handleClose('edit')}
-        initialState={editState.state}
-        editMember={editMember(editState.id)}
-      />
-    </>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell>Role</StyledTableCell>
+            <StyledTableCell onClick={() => setIsAscending(state => !state)}>
+              Create At<StyledButton>{isAscending ? <BsArrowDownShort /> : <BsArrowUpShort />}</StyledButton>
+            </StyledTableCell>
+            <StyledTableCell align="center">Actions</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+          {[...members]
+            .sort(({ createdAt: a }, { createdAt: b }) => {
+              if (a < b) {
+                return isAscending ? 1 : -1;
+              } else if (a > b) {
+                return isAscending ? -1 : 1;
+              } else {
+                return 0;
+              }
+            })
+            .filter(member => {
+              if (admin && member.role === 'ADMIN') return true;
+              if (seller && member.role === 'SELLER') return true;
+              if (buyer && member.role === 'BUYER') return true;
+              return false;
+            })
+            .map(member => (
+              <StyledTableRow key={member.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <StyledTableCell component="th" scope="row">
+                  {member.userId}
+                </StyledTableCell>
+                <StyledTableCell sx={{ textTransform: 'capitalize' }}>
+                  {member.firstName} {member.lastName}
+                </StyledTableCell>
+                <StyledTableCell
+                  color={member.role === 'ADMIN' ? 'warning' : member.role === 'SELLER' ? 'info' : 'success'}>
+                  {member.role}
+                </StyledTableCell>
+                <StyledTableCell>{changeDateFormat(new Date(String(member.createdAt)))}</StyledTableCell>
+                <StyledTableCell align="center">
+                  <Tooltip title="edit" arrow>
+                    <IconButton size="small" data-memberid={member.id} onClick={onClickEditButton} className="margin">
+                      <FaEdit />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="delete" arrow>
+                    <IconButton size="small" data-memberid={member.id} onClick={onClickDeleteButton} className="margin">
+                      <IoMdTrash />
+                    </IconButton>
+                  </Tooltip>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
