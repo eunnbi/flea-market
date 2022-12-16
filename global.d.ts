3663db48b3acd16ba1324fa8bee10ce85cd73bf3
Eunbi <jenabill@naver.com>
2022-12-16 22:35:19 +0900

    add MemberTableState type

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,14 @@
-import type { PrismaClient } from '@prisma/client';
+import type { PrismaClient, User, Product, Wish, Bidding, Image } from '@prisma/client';
 
 declare global {
    var prisma: PrismaClient;
    interface Window {
       kakao: any;
    }
+   type MemberTableState = Pick<User, 'userId' | 'firstName' | 'lastName' | 'role'>;
+   interface ProductItem extends Product {
+      image: Image;
+      wish: Wish;
+      bid: Bidding[];
+    }
 }
-
