98575996030a307f0c03df311d18c5f6c7c7bd7f
Eunbi <jenabill@naver.com>
2022-12-17 16:38:35 +0900

    🩹 fix: Link tag

--- components/WishList.tsx
+++ components/WishList.tsx
@@ -21,67 +21,65 @@ const Item = ({ product }: { product: ProductItem }) => {
   const { id, name, price, status, image, likeCnt, endingAt, bid } = product;
   const endingDate = new Date(String(endingAt));
   return (
-    <StyledLink href={`/products/${id}`} passHref>
-      <article>
-        <div className="imageBox">
-          <ImageWrapper>
-            <Image
-              src={getImageUrl(image)}
-              alt="product thumbnail"
-              layout="fill"
-              placeholder="blur"
-              blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
-            />
-          </ImageWrapper>
-          <Chip
-            label={
-              status === "AUCTION"
-                ? "경매"
-                : status === "PROGRESS"
-                ? "판매 진행중"
-                : "판매 완료"
-            }
-            className="status"
+    <article>
+      <div className="imageBox">
+        <ImageWrapper>
+          <Image
+            src={getImageUrl(image)}
+            alt="product thumbnail"
+            layout="fill"
+            placeholder="blur"
+            blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
           />
-          {status === "AUCTION" && (
-            <Chip
-              label={`D-${getDiffDay(endingDate)}`}
-              className="dday"
-              variant="outlined"
-            />
-          )}
-        </div>
+        </ImageWrapper>
+        <Chip
+          label={
+            status === "AUCTION"
+              ? "경매"
+              : status === "PROGRESS"
+              ? "판매 진행중"
+              : "판매 완료"
+          }
+          className="status"
+        />
+        {status === "AUCTION" && (
+          <Chip
+            label={`D-${getDiffDay(endingDate)}`}
+            className="dday"
+            variant="outlined"
+          />
+        )}
+      </div>
 
-        <div className="wrapper">
-          <h3>{name}</h3>
+      <div className="wrapper">
+        <h3>{name}</h3>
+        <div className="row">
+          {status != "AUCTION" ? (
+            <p className="price">{price.toLocaleString()}원</p>
+          ) : (
+            <p className="price">
+              {bid.length === 0
+                ? "입찰 없음"
+                : `${Math.max(
+                    ...bid.map((elem) => elem.price)
+                  ).toLocaleString()}원`}
+            </p>
+          )}
           <div className="row">
-            {status != "AUCTION" ? (
-              <p className="price">{price.toLocaleString()}원</p>
-            ) : (
-              <p className="price">
-                {bid.length === 0
-                  ? "입찰 없음"
-                  : `${Math.max(
-                      ...bid.map((elem) => elem.price)
-                    ).toLocaleString()}원`}
+            {status === "AUCTION" && (
+              <p className="cnt">
+                <BsFillPeopleFill />
+                <span>{bid.length}</span>
               </p>
             )}
-            <div className="row">
-              {status === "AUCTION" && (
-                <p className="cnt">
-                  <BsFillPeopleFill />
-                  <span>{bid.length}</span>
-                </p>
-              )}
-              <p className="cnt">
-                <IoMdHeart className="heart_icon" />
-                <span>{likeCnt}</span>
-              </p>
-            </div>
+            <p className="cnt">
+              <IoMdHeart className="heart_icon" />
+              <span>{likeCnt}</span>
+            </p>
           </div>
         </div>
-      </article>
-    </StyledLink>
+      </div>
+    </article>
   );
 };
 
