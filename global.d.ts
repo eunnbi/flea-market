5000f5d9f0f9fd9cb5c64b5769fe5e328a43bb8e
Eunbi <jenabill@naver.com>
2022-12-17 16:02:54 +0900

    🏷️ update global types

--- global.d.ts
+++ global.d.ts
@@ -1,14 +1,36 @@
-import type { PrismaClient, User, Product, Wish, Bidding, Image } from '@prisma/client';
+import type {
+  PrismaClient,
+  User,
+  Product,
+  Wish,
+  Bidding,
+  Image,
+  Shopping,
+} from "@prisma/client";
 
 declare global {
-   var prisma: PrismaClient;
-   interface Window {
-      kakao: any;
-   }
-   type MemberTableState = Pick<User, 'userId' | 'firstName' | 'lastName' | 'role'>;
-   interface ProductItem extends Product {
-      image: Image;
-      wish: Wish;
-      bid: Bidding[];
-    }
+  var prisma: PrismaClient;
+  interface Window {
+    kakao: any;
+  }
+  type MemberTableState = Pick<
+    User,
+    "userId" | "firstName" | "lastName" | "role"
+  >;
+  interface ProductItem extends Product {
+    image: Image;
+    wish: Wish;
+    bid: Bidding[];
+  }
+  interface ShoppingItem {
+    item: Shopping;
+    product: ProductItem & {
+      user: User;
+    };
+  }
+  type LoginFormState = Pick<User, "userId" | "password">;
+  type RegisterFormState = Pick<
+    User,
+    "userId" | "password" | "firstName" | "lastName" | "role"
+  >;
 }
