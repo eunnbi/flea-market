10b5424e01322d1a1ca0412abbfe3576c6e07c53
Eunbi <jenabill@naver.com>
2022-12-17 13:15:20 +0900

    ♻️ refactor: register page

--- pages/sell/register.tsx
+++ pages/sell/register.tsx
@@ -1,14 +1,17 @@
-import CustomHead from '@components/common/CustomHead';
-import Header from '@components/common/Header';
-import ProductRegisterForm from '@components/ProductRegisterForm';
-import styled from '@emotion/styled';
-import { getAbsoluteUrl } from '@lib/getAbsoluteUrl';
-import axios from 'axios';
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import CustomHead from "@components/common/CustomHead";
+import Header from "@components/common/Header";
+import ProductRegisterForm from "@components/product/ProductRegisterForm";
+import styled from "@emotion/styled";
+import { getAbsoluteUrl } from "@lib/getAbsoluteUrl";
+import axios from "axios";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
-const ProductRegister = ({ data, token }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const ProductRegister = ({
+  data,
+  token,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   if (token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   }
   return (
     <>
@@ -28,25 +31,30 @@ const Main = styled.main`
   }
 `;
 
-export const getServerSideProps = async ({ req, query }: GetServerSidePropsContext) => {
+export const getServerSideProps = async ({
+  req,
+  query,
+}: GetServerSidePropsContext) => {
   const { cookies } = req;
   const baseUrl = getAbsoluteUrl(req);
   const res = await fetch(`${baseUrl}/api/user/verify`, {
     headers: {
-      Authorization: cookies.access_token ? `Bearer ${cookies.access_token}` : 'Bearer',
+      Authorization: cookies.access_token
+        ? `Bearer ${cookies.access_token}`
+        : "Bearer",
     },
   });
   const { verify, user } = await res.json();
   if (verify) {
-    if (user.role === 'ADMIN') {
+    if (user.role === "ADMIN") {
       return {
         redirect: {
-          destination: '/admin',
+          destination: "/admin",
           permanent: false,
         },
       };
     }
-    if (user.role === 'SELLER') {
+    if (user.role === "SELLER") {
       if (query.id) {
         const res = await fetch(`${baseUrl}/api/product?id=${query.id}`);
         const data = await res.json();
@@ -67,7 +75,7 @@ export const getServerSideProps = async ({ req, query }: GetServerSidePropsConte
   }
   return {
     redirect: {
-      destination: '/',
+      destination: "/",
       permanent: false,
     },
   };
