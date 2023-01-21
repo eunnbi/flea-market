695bbd80df418c0039a5fce141d8f59dcda83856
Eunbi <jenabill@naver.com>
2023-01-21 17:08:27 +0900

    rename: image api

--- pages/api/image/[id].ts
+++ pages/api/image/[id].ts
@@ -1,16 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getImageById, deleteImage } from '@db/image';
-import cloudinary from '@lib/cloudinary';
+import { NextApiRequest, NextApiResponse } from "next";
+import { getImageById, deleteImage } from "@db/image";
+import cloudinary from "@lib/cloudinary";
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     switch (req.method) {
-      case 'DELETE': {
+      case "DELETE": {
         const { id } = req.query;
         const response = await getImageById(id as string);
         await cloudinary.v2.uploader.destroy(response!.publicId, () => {});
-        const image = await deleteImage(id as string);
-        return res.json(image);
+        await deleteImage(id as string);
+        return res.json({ success: true });
       }
       default:
         break;
