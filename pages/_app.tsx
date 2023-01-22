f9daa68a90430a9e619350779ddcc942cfd2ebfd
Eunbi <jenabill@naver.com>
2023-01-22 16:26:52 +0900

    refactor: App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,26 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { theme } from "styles/theme";
-import { useEffect } from "react";
+import { createContext, useEffect } from "react";
 import { RecoilRoot } from "recoil";
 import { Modal } from "@components/common/Modal";
+import Header from "@components/common/Header";
+import axios from "axios";
 
-export default function App({ Component, pageProps, router }: AppProps) {
+interface CommonPageProps {
+  isLogin: boolean;
+  token?: string | null;
+}
+
+export const tokenContext = createContext<string | null | undefined>("");
+
+export default function App({
+  Component,
+  pageProps,
+  router,
+}: AppProps<CommonPageProps>) {
   const { query, pathname } = router;
+  const { isLogin, token } = pageProps;
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -29,11 +43,18 @@ export default function App({ Component, pageProps, router }: AppProps) {
     }
     return () => clearTimeout(timerId);
   }, [query, pathname]);
+
+  axios.defaults.headers.common["Authorization"] = token
+    ? `Bearer ${token}`
+    : "";
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <Modal />
+        <tokenContext.Provider value={token}>
+          <Header isLogin={isLogin} pathname={pathname} query={query} />
+          <Component {...pageProps} />
+          <Modal />
+        </tokenContext.Provider>
       </ThemeProvider>
     </RecoilRoot>
   );
